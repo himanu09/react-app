c5f3d40d7b526c683a708879c311013bab4e4b0e
Himanshu Rajore <hhimanshurajore@gmail.com>
2025-12-03 10:29:12 +0530

    Manually added react and npn in project.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# React App 🎯 
+# React App 🎯
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting 
+- Differential Bundling -support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shacking - remove unused code
+- Different Dev and Prod bundles
--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const parent = React.createElement("div", { id: "parent" }, 
-                                    React.createElement("div", { id: "child" },
-                                      [React.createElement("h1", { id: "grandchild1" }, "Hello, World By Parent and Child!"),
-                                      React.createElement("h2", { id: "grandchild2" }, "Hello, World By Parent and Grandchild!")]
-                                    )
-                                  );
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child", key: "child" },
+    [
+      React.createElement("h1", { id: "grandchild1", key: "g1" }, "Hello, World By Parent and Child!"),
+      React.createElement("h2", { id: "grandchild2", key: "g2" }, "Hello, World By Parent and Grandchild!"),
+      React.createElement("h3", { id: "grandchild3", key: "g3" }, "Hello, World By  GrandChild3!"),
+      React.createElement("h4", { id: "grandchild4", key: "g4" }, "Hello, World By  GrandChild4!")
+    ]
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
